252302513fc5fa2fc44878b5f96a8e792f5bc495
Maxwellt <maksimvinnik@gmail.com>
2020-09-28 18:49:33 +0300

    init

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public GameObject tilePrefab;
+
+    private Tile[,] _allTiles;
+    private void Start()
+    {
+        _allTiles = new Tile[width, height];
+        SetupTiles();
+    }
+
+    private void Update()
+    {
+   
+    }
+
+    private void SetupTiles()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity);
+                tile.name = "Tile (" + i + "," + j + ")";
+                _allTiles[i, j] = tile.GetComponent<Tile>();
+                tile.transform.parent = transform;
+            }
+        }
+    }
+}
